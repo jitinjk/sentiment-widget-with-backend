f6b8cce3e7629ab1d7407f01e14cfc0b075d19b6
jitin jayakumar <jitinjayakumar@gmail.com>
2026-04-22 11:49:44 +0800

    Bootstrap Spring Boot application in backend folder

---
+++ backend/src/main/java/com/example/sentimentbackend/SentimentBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.sentimentbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SentimentBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SentimentBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/example/sentimentbackend/SentimentBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sentimentbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SentimentBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
